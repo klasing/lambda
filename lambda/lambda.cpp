1d3dc176f7ae8fca3cb0b22cb3e9e6d83b34ab02
klasing <klasing1959@gmail.com>
2019-10-12 09:59:08 +0200

    bla

--- lambda/lambda.cpp
+++ lambda/lambda.cpp
@@ -6,6 +6,7 @@
 #include <conio.h>
 #include <string>
 #include <memory>
+#include <functional>
 
 int main()
 {
@@ -26,7 +27,7 @@ int main()
 	{
 		str = "captured";
 		return true;
-	} ();
+	} (); // call the lambda immediately
 	std::cout << "A) modify capture...............: " << str << std::endl;
 
 	auto pstr = std::make_unique<std::string>(str);
@@ -43,6 +44,47 @@ int main()
 	};
 	void (*pfunc)(std::string) = lambda;
 	pfunc(str_return);
+	std::cout << "D) lambda as function parameter\n";
+
+	std::cout << "E) constexpr\n";
+	// a constexpr is a expression that can be evaluated at compile time
+	// it has an advantage in performance over am expression that is evaluated at
+	// run time
+	int const x = 10, y = 20;
+	constexpr int z = x * y;
+	std::cout << "   z...: " << z << std::endl;
+	// can't make a lambda explicitly a constexpr, 
+	// this has to do with the conformance mode of the compiler (/std:c++17)
+	// the following lambda is a implicitly constexpr lambda
+	// the compiler marks the lambda with a comment:
+	// a lambda expression is not allowed in a constant expression
+	// (so there will be undefined behaviour, i assume)
+	constexpr auto a = [x, y]() { return x * y; };
+	std::cout << "   a...: " << z << std::endl;
+
+	std::cout << "EXAMPLE 1\n";
+	std::cout << "assign the lambda expression that adds two numbers to an auto variable\n";
+	auto f1 = [](int x, int y) { return x + y; };
+	std::cout << "1 + 2 = " << f1(1, 2) << std::endl;
+	std::cout << "assign the same lambda expression to a function object\n";
+	std::function<int(int, int)> f2 = [](int x, int y) { return x + y; };
+	std::cout << "2 + 3 = " << f2(2, 3) << std::endl;
+
+	std::cout << "EXAMPLE 2\n";
+	std::cout << "the following lambda expression captures 'a_' by value and 'b_' by reference\n";
+	int a_ = 1;
+	int b_ = 2;
+	std::function<int()> f = [a_, &b_]() { return a_ + b_; };
+	// change the values of 'a_' and 'b_'
+	a_ = 2;
+	b_ = 3;
+	std::cout << "a_ + b_ = " << f() << std::endl;
+
+	std::cout << "EXAMPLE 3\n";
+	// use a lambda expression as a paramter in a function
+	// TODO
+
+	// spaceship comparison <=>, only in C++20
 
 	std::cout << "\n";
 	std::cout << "Hit any key ...";
